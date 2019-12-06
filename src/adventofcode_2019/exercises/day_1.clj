b572da5d153691ddef5029b180371ba256da34ff
Leonardo Freitas <leonardo.freitas.s@outlook.com>
2019-12-06 16:44:34 +0100

    Setting basic exercise handler

--- src/adventofcode_2019/exercises/day_1.clj
+++ src/adventofcode_2019/exercises/day_1.clj
@@ -1,6 +1,10 @@
 (ns adventofcode-2019.exercises.day-1)
 
+(defn calculate-fuel [mass]
+  (- (Math/ceil (/ mass 3))
+     2))
+
 (defn run
   "Day one exercise"
   [inputs]
-  (println inputs))
+  (reduce (fn [a b] (+ a (calculate-fuel b))) 0))
