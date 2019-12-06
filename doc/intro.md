a9de7f68b00575c214e7d023f3ac6c827b1f2d98
Leonardo Freitas <leonardo.freitas.s@outlook.com>
2019-12-06 11:29:00 +0100

    Initial leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to adventofcode-2019
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/adventofcode_2019/core.clj
@@ -0,0 +1,7 @@
+(ns adventofcode-2019.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
